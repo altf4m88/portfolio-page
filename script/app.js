20c06034bf580f6d36180c5bf761fa861937cf74
altf4m88 <fadhilmauladhani@gmail.com>
2021-01-07 19:34:34 +0700

    delete unused code line

--- script/app.js
+++ script/app.js
@@ -4,14 +4,9 @@ const timelineMax = new TimelineMax();
 const hero = document.querySelector(".hero");
 const headline = document.querySelector(".headline");
 const secondaryheadline = document.querySelector(".secondary-headline");
-const navtext = document.querySelector(".nav-text");
 const navlinks = document.querySelector(".nav-links");
-const socialmedia = document.querySelector(".social-media");
 
 timelineMax.fromTo(hero, 3, {height: "0%"}, {height: "100%", ease:Power2.easeInOut})
 .fromTo(headline, 1, {opacity: 0, x: 150, left: "0%"}, {opacity: 1, x:0, left: 40 })
 .fromTo(secondaryheadline, 1, {opacity: 0, x: 150, left: "0%"}, {opacity: 1, x:0, left: 40 })
-.fromTo(navtext, 1, {opacity: 0}, {opacity: 1})
-.fromTo(navlinks, 1, {opacity: 0}, {opacity: 1})
-.fromTo(socialmedia, 1, {opacity: 0}, {opacity: 1});
-
+.fromTo(navlinks, 1, {opacity: 0}, {opacity: 1});
