a5fb9bc956299ef219f94f59a6453b3240229e88
altf4m88 <fadhilmauladhani@gmail.com>
2021-01-07 20:04:23 +0700

    test on scroll event

--- script/app.js
+++ script/app.js
@@ -10,3 +10,10 @@ timelineMax.fromTo(hero, 3, {height: "0%"}, {height: "100%", ease:Power2.easeInO
 .fromTo(headline, 1, {opacity: 0, x: 150, left: "0%"}, {opacity: 1, x:0, left: 40 })
 .fromTo(secondaryheadline, 1, {opacity: 0, x: 150, left: "0%"}, {opacity: 1, x:0, left: 40 })
 .fromTo(navlinks, 1, {opacity: 0}, {opacity: 1});
+
+
+const project = document.querySelector("#projects");
+
+project.addEventListener("scroll", () => {
+    console.log("test");
+})
